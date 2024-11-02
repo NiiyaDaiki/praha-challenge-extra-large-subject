ff48d67f55ba185d857adb00a609ab6dc3955bf4
NiiyaDaiki <56012694+NiiyaDaiki@users.noreply.github.com>
2024-11-03 00:02:42 +0900

    fix:pairがmemberの情報を直接持たないように変更

--- backend/src/domain/entity/pair.ts
+++ backend/src/domain/entity/pair.ts
@@ -1,19 +1,18 @@
-import { Participant } from './participant'
+
 
 export class Pair {
   readonly id: string
   readonly name: string
-  readonly participants: Participant[]
+  readonly participantIds: string[]
 
-  constructor(args: { id: string; name: string; participants: Participant[] }) {
-    const { id, name, participants } = args
+  constructor(args: { id: string; name: string; participantIds: string[] }) {
+    const { id, name, participantIds } = args
     this.isValidName(name)
-    this.isValidNumberOfParticipants(participants)
-    this.areParticipantsAllActive(participants)
+    this.isValidNumberOfParticipants(participantIds)
 
     this.id = id
     this.name = name
-    this.participants = participants
+    this.participantIds = participantIds
   }
 
   private isValidName(name: string) {
@@ -23,17 +22,9 @@ export class Pair {
     }
   }
 
-  private isValidNumberOfParticipants(participants: Participant[]) {
-    if (participants.length < 2 && participants.length > 3) {
+  private isValidNumberOfParticipants(participantIds: string[]) {
+    if (participantIds.length < 2 && participantIds.length > 3) {
       throw new Error('ペアには2名〜3名の参加者が必要です。')
-    }
-  }
-
-  private areParticipantsAllActive(participants: Participant[]) {
-    if (!participants.every((participants) => participants.isActive())) {
-      throw new Error(
-        'すべての参加者がペアに参加できる状態でなければなりません。',
-      )
     }
   }
 }
