4259ec62412d85c8569e94bce433d653ba8dbe0f
NiiyaDaiki <56012694+NiiyaDaiki@users.noreply.github.com>
2024-11-09 17:23:56 +0900

    feat:削除処理ロジックを追加

--- backend/src/domain/entity/pair.ts
+++ backend/src/domain/entity/pair.ts
@@ -3,28 +3,57 @@
 export class Pair {
   readonly id: string
   readonly name: string
-  readonly participantIds: string[]
+  private _participantIds: string[]
 
   constructor(args: { id: string; name: string; participantIds: string[] }) {
     const { id, name, participantIds } = args
     this.isValidName(name)
-    this.isValidNumberOfParticipants(participantIds)
-
     this.id = id
     this.name = name
-    this.participantIds = participantIds
+    this._participantIds = participantIds
+    this.isValidNumberOfParticipants()
+
   }
 
   private isValidName(name: string) {
-    const regex = /[^[a-zA-Z]$]/
+    const regex = /^[a-zA-Z]$/
     if (!regex.test(name)) {
-      throw new Error('ペア名はa-eの英文字1文字でなければなりません。')
+      throw new Error('ペア名はa-zの英文字1文字でなければなりません。')
     }
   }
 
-  private isValidNumberOfParticipants(participantIds: string[]) {
-    if (participantIds.length < 2 && participantIds.length > 3) {
+  private isValidNumberOfParticipants() {
+    console.log(`現在のペア${this.name}の人数: ${this.getParticipantIds().length}`)
+    if (this.getParticipantIds().length < 2 || this.getParticipantIds().length > 3) {
       throw new Error('ペアには2名〜3名の参加者が必要です。')
     }
+  }
+
+  getParticipantIds(): string[] {
+    return this._participantIds
+  }
+
+  public addParticipant(memberId: string): void {
+    if (this.getParticipantIds().includes(memberId)) {
+      throw new Error('既にPairに所属しています。');
+    }
+    this.getParticipantIds().push(memberId);
+    try {
+      this.isValidNumberOfParticipants();
+    } catch (e) {
+      console.log('ペアの許容人数を超えました')
+      throw e
+    }
+  }
+
+  public removeParticipant(memberId: string): void {
+    this._participantIds = this.getParticipantIds().filter(id => id !== memberId);
+    if (this.getParticipantIds().length === 0) {
+      throw new Error('Pairのメンバーが0名になりました。');
+    }
+  }
+
+  public isParticipantExist = (participantId: string): boolean => {
+    return this.getParticipantIds().includes(participantId)
   }
 }
