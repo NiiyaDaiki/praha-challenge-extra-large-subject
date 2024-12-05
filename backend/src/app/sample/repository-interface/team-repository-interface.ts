2b1fcf5e86950c463c7b561f25eb5ee96fbf7235
NiiyaDaiki <56012694+NiiyaDaiki@users.noreply.github.com>
2024-12-05 22:55:00 +0900

    feat:チーム間のペア移動機能

--- backend/src/app/sample/repository-interface/team-repository-interface.ts
+++ backend/src/app/sample/repository-interface/team-repository-interface.ts
@@ -3,6 +3,7 @@ import { Team } from "../../../domain/entity/team";
 export interface ITeamRepository {
   findAll(): Promise<Team[]>;
   findById(id: string): Promise<Team | undefined>;
+  findByPairId(pairId: string): Promise<Team | null>
   findByParticipantId(id: string): Promise<Team | undefined>;
   save(team: Team): Promise<void>;
 }
--- backend/src/infra/db/repository/team-repository.ts
+++ backend/src/infra/db/repository/team-repository.ts
@@ -67,6 +67,38 @@ export class TeamRepository implements ITeamRepository {
     });
   }
 
+  public async findByPairId(pairId: string): Promise<Team | null> {
+    const pair = await this.prismaClient.pair.findUnique({
+      include: {
+        team: {
+          include: {
+            pairs: {
+              include: {
+                participants: true
+              }
+            }
+          }
+        }
+      },
+      where: { id: pairId }
+    })
+    if (!pair || !pair.team) {
+      return null
+    }
+
+    return new Team({
+      id: pair.team.id,
+      name: pair.team.name,
+      pairs: pair.team.pairs.map((pair) => {
+        return new Pair({
+          id: pair.id,
+          name: pair.name,
+          participantIds: pair.participants.map((participant) => participant.id)
+        })
+      })
+    })
+  }
+
   public async findByParticipantId(participantId: string): Promise<Team | undefined> {
     const participant = await this.prismaClient.participant.findUnique({
       include: {
